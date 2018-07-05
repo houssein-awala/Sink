22492a160d4f5263d4da28134a7a65d9cd3ac2b3
mostafa <40169158+motstafa@users.noreply.github.com>
2018-07-06 01:00:48 +0300

    Update connectionWithRouterForGetRequestResult.java

--- src/sink/connectionWithRouterForGetRequestResult.java
+++ src/sink/connectionWithRouterForGetRequestResult.java
@@ -5,7 +5,12 @@
  */
 package sink;
 
+import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -15,9 +20,10 @@ import java.io.ObjectInputStream;
 public class connectionWithRouterForGetRequestResult extends Thread
 {
     ObjectInputStream reader ;
-
-    public connectionWithRouterForGetRequestResult(ObjectInputStream reader) {
+    ObjectOutputStream out;
+    public connectionWithRouterForGetRequestResult(ObjectInputStream reader,ObjectOutputStream out) {
         this.reader = reader;
+        this.out=out;
     }
 
 
@@ -27,7 +33,18 @@ public class connectionWithRouterForGetRequestResult extends Thread
     
     @Override
     public void run(){
-    
+        try {
+            SinkRequest request= (SinkRequest)reader.readObject();
+            int requestid=request.requestID;
+            if(Sink.Requests.containsKey(requestid))                    
+            {
+                RC4Security sec = new RC4Security();
+               String result =  sec.decrypt(Sink.Requests.get(requestid),"RC4",request.result);
+              System.out.println(result);
+              Sink.Requests.remove(requestid);            }
+                    } catch (IOException | ClassNotFoundException ex) {
+            Logger.getLogger(connectionWithRouterForGetRequestResult.class.getName()).log(Level.SEVERE, null, ex);
+        }
     
     }
 }
